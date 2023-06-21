1cded8248a8345bdb699363c62977922452664f8
Eleni <elech2a@gmail.com>
2023-06-20 19:30:54 -0700

    Find percentage of male and female students

--- main.py
+++ main.py
@@ -11,6 +11,24 @@ def main():
     # Do not delete the return statement
     ########################################
     """
+
+    #Set female and male variables
+    male = int(input('Input the number of males in the class: '))
+    fem = int(input('Input the number of females in the class: '))
+
+
+    #Calculate the total number of students
+    total = fem + male
+
+    #Find the percentage of female and male students
+    f_perc = (fem/total)
+    m_perc = (male/total)
+
+    #Print all the values
+    print(f'\nThe total number of students: \t {total}')
+    print(f'The number of males and females: \t {male} \t {fem}')
+    print(f'The percentage of males and females: \t {m_perc:.2%} \t {f_perc:.2%}')
+
     return m_perc, f_perc
 
 
