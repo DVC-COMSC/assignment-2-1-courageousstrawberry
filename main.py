853001ba7334d040adbbdbf16a83a4865937ffe2
Eleni <elech2a@gmail.com>
2023-06-21 10:25:41 -0700

    change variable name

--- main.py
+++ main.py
@@ -13,20 +13,20 @@ def main():
     """
 
     #Set female and male variables
-    male = int(input('Input the number of males in the class: '))
-    fem = int(input('Input the number of females in the class: '))
+    m = int(input('Input the number of males in the class: '))
+    f = int(input('Input the number of females in the class: '))
 
 
     #Calculate the total number of students
-    total = fem + male
+    total = f + m
 
     #Find the percentage of female and male students
-    f_perc = (float(fem)/total)
-    m_perc = (float(male)/total)
+    f_perc = (float(f)/total)
+    m_perc = (float(m)/total)
 
     #Print all the values
     print(f'\nThe total number of students: \t {total}')
-    print(f'The number of males and females: \t {male} \t {fem}')
+    print(f'The number of males and females: \t {m} \t {f}')
     print(f'The percentage of males and females: \t {m_perc:.2%} \t {f_perc:.2%}')
 
     return m_perc, f_perc
